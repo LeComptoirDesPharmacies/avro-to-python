e2b17e9c37d124a68615cbe95bbe109bb6093047
Antoine LCDP <a.brultet@lecomptoirdespharmacies.fr>
2022-06-10 10:52:30 +0200

    Revert "Update version"
    This reverts commit c2ab5b63b34b71b8f503c87f526a4c0dd004b3e2.

--- setup.py
+++ setup.py
@@ -60,6 +60,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/LeComptoirDesPharmacies/avro-to-python',
-    version='0.3.3-7',
+    version='0.3.3-6',
     zip_safe=False,
 )
